2150bce085791dc3f482080528acbd7d03bf22d2
Terry chan <napoler2008@gmail.com>
2021-08-15 14:33:23 +0800

    auto更新文档

--- docs_tools/source/conf.py
+++ docs_tools/source/conf.py
@@ -26,7 +26,7 @@ author =author
 # The full version, including alpha/beta/rc tags
 release = version
 
-
+version = version
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
