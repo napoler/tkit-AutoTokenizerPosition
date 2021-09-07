2a1c804a19a1cadfcd3df26b74054ce209161d13
Terry Chan <napoler2008@gmail.com>
2021-09-07 10:03:47 +0800

    0.04修正位置处理错误

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 name='tkitAutoTokenizerPosition'#修改包名字-
-version='0.0.0.2 '
+version='0.0.0.4'
 description='Terry toolkit tkitAutoTokenizerPosition'
 author='Terry Chan'
 author_email='napoler2008@gmail.com'
-url='http://www.terrychan.org/tkit-AutoTokenizerPosition/'
+url='https://lab.terrychan.org/tkit-AutoTokenizerPosition/'
 
 copyright = '2021, Terry Chan'
 language = 'zh_cn'
